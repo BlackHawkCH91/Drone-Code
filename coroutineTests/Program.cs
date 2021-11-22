9f1581a1b703a3717baeb97b38827a9c421b0633
BlackHawkCH91 <49353890+BlackHawkCH91@users.noreply.github.com>
2021-11-22 23:21:47 +1000

    Merge

---
+++ coroutineTests/Program.cs
@@ -0,0 +1,55 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+
+        public Program()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            Coroutine.AddCoroutine(new someshit(TestThing), "hello", "world");
+        }
+
+        public delegate IEnumerator<int> someshit(string shit1, string shit2);
+        public IEnumerator<int> TestThing(string text1, string text2)
+        {
+            while (true)
+            {
+                Echo(text1);
+                yield return 10;
+                Echo(text2);
+                yield return 10;
+            }
+            
+        }
+
+    }
+}
