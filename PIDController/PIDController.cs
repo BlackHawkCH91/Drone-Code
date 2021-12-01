913eb4294f81b300797bd7c2cf7f3d471db30af4
Mr Mudman <mrmudman123@gmail.com>
2021-12-01 20:52:32 +1000

    Update movement code to use new PID controller class

--- PIDController/PIDController.cs
+++ PIDController/PIDController.cs
@@ -1,41 +1,59 @@
-﻿using System;
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
 
-namespace PIDController
+namespace IngameScript
 {
-    class PIDController<T>
+    partial class Program
     {
-        // Gain vars
-        private double pGain;
-        private double iGain;
-        private double dGain;
-
-        // Integral / Derivative vars
-        private int maxErrorMemory;
-        T[] errorVals;
-
-        // Private methods
-        private void addToErrorMemory(T errorVal)
+        public class PIDController<T>
         {
+            // Gain vars
+            private double pGain;
+            private double iGain;
+            private double dGain;
 
-        }
+            // Integral / Derivative vars
+            private int maxErrorMemory;
+            T[] errorVals;
 
-        // Public methods
-        public T PIDController.getControlValue(T errorVal)
-        {
-            // Add error value to memory
-            addToErrorMemory(errorVal);
+            // Private methods
+            private void addToErrorMemory(T errorVal)
+            {
 
-            // Get P value
-            T pVal = errorVal * pGain;
+            }
 
-            // Get I value
+            // Public methods
+            public T PIDController.getControlValue(T errorVal)
+            {
+                // Add error value to memory
+                addToErrorMemory(errorVal);
 
-        }
+                // Get P value
+                T pVal = errorVal * pGain;
 
-        // Constructors
-        public PIDController<T>(double PGain, double IGain, double DGain, int MaxErrorMemory)
+                // Get I value
+
+            }
+
+            // Constructors
+            public PIDController<T>(double PGain, double IGain, double DGain, int MaxErrorMemory)
         {
             // Set gain vars
             pGain = PGain;
@@ -47,4 +65,5 @@ namespace PIDController
             errorVals = new Dictionary<double, T>(MaxErrorMemory);
         }
     }
+}
 }
