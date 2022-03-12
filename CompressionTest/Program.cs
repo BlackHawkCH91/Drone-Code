3d7f443e90153d33f68a673af8fbd104482219b5
BlackHawkCH91 <49353890+BlackHawkCH91@users.noreply.github.com>
2022-03-12 23:15:25 +1000

    got 1 mb down to 56 kb

--- CompressionTest/Program.cs
+++ CompressionTest/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CompressionTest
 {
     class Program
     {
         static Dictionary<string, string> colourKeys = new Dictionary<string, string>();
-        static List<string> availableChars = new List<string>(new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", ",", "<", "." });
+        static List<string> availableChars = new List<string>(new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", ",", "<", "." });
 
 
         static void compressFile()
@@ -25,10 +27,14 @@ namespace CompressionTest
                 {
                     if (texts[i] != "1")
                     {
-                        if ((int.Parse(texts[i]) >= 10 && int.Parse(texts[i]) <= 255) || int.Parse(texts[i]) >= 1000)
+                        if (int.Parse(texts[i]) >= 10 && int.Parse(texts[i]) <= 255)
                         {
                             sw.Write(int.Parse(texts[i]).ToString("X"));
-                        }
+                        } 
+                        else if (int.Parse(texts[i]) >= 1000)
+                        {
+                            sw.Write(int.Parse(texts[i]).ToString("X4"));
+                        } 
                         else
                         {
                             sw.Write(texts[i]);
@@ -40,12 +46,66 @@ namespace CompressionTest
 
                     if (!colourKeys.ContainsKey(texts[i + 1]))
                     {
-                        Console.WriteLine(texts[i + 1]);
+                        Console.WriteLine(texts[i + 1] + " | " + availableChars[character]);
                         colourKeys.Add(texts[i + 1], availableChars[character]);
                         character++;
                     }
                     sw.Write(colourKeys[texts[i + 1]]);
                 }
+            }
+            //inverseColourKeys = new Dictionary<string, string>(colourKeys.Values, colourKeys.Keys);
+        }
+
+        static void compressFile2()
+        {
+            StringBuilder final = new StringBuilder();
+            int maxAttempt = 30;
+            string result = File.ReadAllText(@"D:\SE-PlanetMapping\result2.txt");
+            string repeatSection = "";
+            int compressCount = 1;
+
+            using (StreamWriter sw = File.CreateText(@"D:\SE-PlanetMapping\compressTest.txt"))
+            {
+                for (int i = 0; i < result.Length; i += repeatSection.Length * compressCount)
+                {
+                    repeatSection = "";
+                    compressCount = 1;
+                    for (int attempt = 0; attempt < maxAttempt && compressCount == 1 && i + attempt < result.Length; attempt++)
+                    {
+                        repeatSection += result[i + attempt];
+                        //Console.WriteLine(repeatSection);
+
+                        while (true)
+                        {
+                            try
+                            {
+                                if (repeatSection == result.Substring(i + repeatSection.Length * compressCount, repeatSection.Length))
+                                {
+                                    compressCount++;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (compressCount > 1)
+                    {
+                        final.Append("?" + compressCount + repeatSection.Length + "|");
+                    } else
+                    {
+                        repeatSection = result[i].ToString();
+                    }
+
+                    final.Append(repeatSection);
+                }
+                sw.Write(final.ToString());
             }
         }
 
@@ -53,19 +113,38 @@ namespace CompressionTest
         {
             string result2 = File.ReadAllText(@"D:\SE-PlanetMapping\result2.txt");
 
-            string start = result2.Substring(0, 9);
+            string start = result2.Substring(0, 8) + "$";
             string contents = result2.Substring(9, result2.Length - 9);
 
             using (StreamWriter sw = File.CreateText(@"D:\SE-PlanetMapping\result3.txt"))
             {
+                sw.Write(start + ",");
+                string number = "";
+                foreach (char character in contents)
+                {
+                    if (!availableChars.Contains(character.ToString()))
+                    {
+                        number += character;
+                    } else
+                    {
+                        int test = 1;
+                        if (number.Length > 0)
+                        {
+                            test = number.Length % 2 == 0 ? int.Parse(number, System.Globalization.NumberStyles.HexNumber) : int.Parse(number);
+                        }
+                        sw.Write($"{test},{colourKeys.FirstOrDefault(x => x.Value == character.ToString()).Key},");
 
+                        number = "";
+                    }
+                }
             }
         }
 
         static void Main(string[] args)
         {
             compressFile();
-
+            compressFile2();
+            uncompressFile();
 
             //string result2 = File.ReadAllText(@"D:\SE-PlanetMapping\result2.txt");
             //Console.WriteLine($"{result.Length} characters to {result2.Length} characters. {result.Length - result2.Length} difference.");
