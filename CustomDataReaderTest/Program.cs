ac0c760f95b294bdcb9a57fb9434b729b0501ff9
Mr Mudman <mrmudman123@gmail.com>
2022-04-15 01:49:47 +1000

    help it wont let me commitno idea

---
+++ CustomDataReaderTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+
+        public Program()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            Echo("actually working");
+            Echo(((int)Me.CustomData[0] + (int)Me.CustomData[1] - 64).ToString());
+        }
+    }
+}
---
+++ PlanetOreDataReader/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace PlanetOreDataReader
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "JUST SHUT UP WILL ONLY BE RUN ON WINDOWS")]
+    class Program
+    {
+
+        
+        static Bitmap bmp = new Bitmap("testmap.bmp");
+        static StreamWriter sw = File.CreateText("result.txt");
+
+        static byte[,] pixelData = new byte[bmp.Width, bmp.Height];
+        static StringBuilder stringBuilder = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            var data = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+            var depth = Bitmap.GetPixelFormatSize(data.PixelFormat) / 8; //bytes per pixel
+
+            var buffer = new byte[data.Width * data.Height * depth];
+
+            //copy pixels to buffer
+            Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+
+            Console.WriteLine("Starting");
+            stringBuilder.Append($"{bmp.Height}{bmp.Width}$");
+
+            Parallel.For(0, data.Width, x => {         
+                Parallel.For(0, data.Height, y =>
+                {
+                    // Grabs data from blue channel (every 3rd byte)
+                    pixelData[x, y] = buffer[data.Stride * y + depth * x];    
+                }); 
+            });
+
+            byte? prevByte = null;
+            int numSameBytes = 0;
+            for(int y = 0; y < pixelData.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x < pixelData.GetUpperBound(0); x++)
+                {
+                    if (pixelData[x,y] == prevByte)
+                    {
+                        numSameBytes++;
+                    }
+                    else if (prevByte != null)
+                    {
+                        stringBuilder.Append($",{numSameBytes},{prevByte.ToString().PadLeft(3, '0')}");
+                        prevByte = pixelData[x, y];
+                        numSameBytes = 1;
+                    }
+                    else
+                    {
+                        prevByte = pixelData[x, y];
+                        numSameBytes = 1;
+                    }
+                }
+            }
+
+            bmp.UnlockBits(data);
+            sw.Write(stringBuilder.ToString());
+            Console.WriteLine("Complete, result path: ");
+            Console.WriteLine(Directory.GetCurrentDirectory() + @"\result.txt");
+        }
+
+
+    }
+}
