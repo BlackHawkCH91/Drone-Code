bbf328e6559b11263db04fcfe3b706ae50220751
BlackHawkCH91 <christian.hensman1@gmail.com>
2022-04-25 00:27:59 +1000

    Moved more methods into shared

--- NetworkMethods/network.cs
+++ NetworkMethods/network.cs
@@ -89,6 +89,10 @@ namespace IngameScript
                 return position;
             }
 
+            //----------------------------------------------------------------------------
+
+
+
             //!Gets positions of [] in strings
             public static Dictionary<string, List<int>> GetBracketPos(string packet)
             {
@@ -114,6 +118,190 @@ namespace IngameScript
                 }
 
                 return bracketPos;
+            }
+
+            //!Converts object to string
+            public static string ObjectToString(object[] packet)
+            {
+                //objToStrConverter(packet, ref final);
+
+                string final = "[";
+                int i = 0;
+
+                //Loop through all items in object
+                foreach (object item in packet)
+                {
+                    if (item.GetType() == typeof(object[]))
+                    {
+                        //If it is an object array, use recursion
+                        final += ObjectToString((object[])item);
+                    }
+                    else
+                    {
+                        //If not, convert type to string. If its a string, add "", if vec3 use toString, etc
+                        switch (item.GetType().ToString())
+                        {
+                            case "System.String":
+                                final += "\"" + item + "\"";
+                                break;
+                            case "System.DateTime":
+                                final += "D" + item.ToString();
+                                break;
+                            case "System.Boolean":
+                                final += item.ToString();
+                                break;
+
+                            case "VRageMath.Vector3D":
+                                Vector3D vec3 = (Vector3D)item;
+                                final += vec3.ToString();
+                                break;
+
+                            case "VRageMath.MatrixD":
+                                final += MatrixToString((MatrixD)item);
+                                break;
+
+                            default:
+                                final += item;
+                                break;
+                        }
+                    }
+
+                    //Prevent adding unnecessary comma
+                    if (i < packet.Length - 1)
+                    {
+                        final += ",";
+                    }
+
+                    i++;
+                    //TaskScheduler.SpawnCoroutine(new Func<IEnumerator<int>>(yieldThing)));
+                }
+
+                final += "]";
+
+                return final;
+            }
+
+            //!Converts string back into object
+            public static object[] StringToObject(string packet)
+            {
+                //Remove start and ending brackets
+                packet = packet.Substring(1, packet.Length - 2);
+
+                //Dictionaries to store bracket pos and linked brackets
+                Dictionary<int, int> linked = new Dictionary<int, int>();
+                Dictionary<string, List<int>> bracketPos = GetBracketPos(packet);
+
+                //Find linked brackets
+                foreach (int closePos in bracketPos["]"])
+                {
+                    linked.Add(closePos, 0);
+                    foreach (int openPos in bracketPos["["])
+                    {
+                        //Loop through opening and closing brackets. If the open bracket had not been taken and is closer than the last,
+                        //add/change linked
+                        if (closePos > openPos && openPos > linked[closePos] && !(linked.ContainsValue(openPos)))
+                        {
+                            linked[closePos] = openPos;
+                        }
+                    }
+                }
+
+                //Get fields that are parents, ignore their children
+                List<int> parents = new List<int>();
+
+                //Loop through all linked brackets
+                foreach (int potChild in linked.Keys)
+                {
+                    bool isChild = false;
+
+                    //potChild = potential child, ignore terrible var name
+
+                    //Loop through all linked brackets, checking if potChild is a child
+                    foreach (KeyValuePair<int, int> parent in linked)
+                    {
+                        if (potChild < parent.Key && potChild > parent.Value)
+                        {
+                            isChild = true;
+                            break;
+                        }
+                    }
+
+                    //if potChild is false (not a child), add to parent list
+                    if (!(isChild))
+                    {
+                        parents.Add(potChild);
+                    }
+                }
+
+                //Recursively run function to convert substrings (children) into objects
+                int difference = 0;
+                List<object[]> subObjectArr = new List<object[]>();
+
+                foreach (int item in parents)
+                {
+                    //Get section of string that is an object, remove it from packet, and do recursion
+                    string subObject = packet.Substring(linked[item] - difference, item - linked[item] + 1);
+                    packet = packet.Remove(linked[item] - difference, subObject.Length);
+                    difference += subObject.Length;
+
+                    subObjectArr.Add(StringToObject(subObject));
+
+                }
+
+                //
+                //Creating two arrays here. Is it possible to use just one?
+                //
+
+                //Convert the string into an object array
+                object[] packetArr = packet.Split(',');
+                object[] finalPacketArr = new object[packetArr.Length];
+                int a = 0;
+
+                for (int i = 0; i < packetArr.Length; i++)
+                {
+                    string item = packetArr[i] as string;
+
+                    //Convert values into correct variable types
+                    if (item.Length > 0)
+                    {
+                        //Order: Vector3, String, Double, Int, Subobject
+                        if (item[0] == '{' || item[0] == 'X')
+                        {
+                            finalPacketArr[i] = StringToVector3(item);
+                        }
+                        else if (item[0] == '"')
+                        {
+                            finalPacketArr[i] = item.Substring(1, item.Length - 2);
+                        }
+                        else if (item.StartsWith("T") || item.StartsWith("F")) //New thing. Needs testing
+                        {
+                            finalPacketArr[i] = item.StartsWith("T"); //
+                        }
+                        else if (item[0] == 'M') // Another new thing
+                        {
+                            finalPacketArr[i] = StringToMatrix(item);
+                        }
+                        else if (item[0] == 'D')
+                        {
+                            finalPacketArr[i] = DateTime.Parse(item);
+                        }
+                        else if (item.Contains("."))
+                        {
+                            finalPacketArr[i] = Convert.ToDouble(item);
+                        }
+                        else
+                        {
+                            finalPacketArr[i] = long.Parse(item);
+                        }
+                    }
+                    else
+                    {
+                        finalPacketArr[i] = subObjectArr[a];
+                        a++;
+                    }
+                }
+
+                return finalPacketArr;
             }
         }
     }
